f5b0d637067feb63e082e3c238fd293306a20198
GBTC405002UR <GBTC405002UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-22 16:27:02 -0400

    First part of the Bullhorn project

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=4)
+    private String content;
+
+    @NotNull
+    private String dueDate;
+
+    @NotNull
+    @Size(min=4)
+    private String sentby;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public String getSentby() {
+        return sentby;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setSentby(String sentby) {
+        this.sentby = sentby;
+    }
+}
+
